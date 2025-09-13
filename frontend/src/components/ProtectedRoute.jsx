9ac3fef7d73680b6efd75bba26b693ff0e01cff1
Lunado <liendhhha140217@gmail.com>
2025-09-13 22:55:39 +0700

    add reusable Form.jsx for login & register

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,46 @@
+import {Navigate} from 'react-router-dom'
+import {jwtDecode} from 'jwt-decode'
+import api from "../api"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants'
+import { useState } from 'react'
+
+function ProtectedRoute({children}){
+    const [isAuthorized, setIsAuthorized] = useState(null)
+
+    useEffect (() => {
+        auth().catch() => setIsAuthorized(false)
+    }, [])
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try {
+            
+        }
+    }
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if (!token){
+            setIsAuthorized(false)
+            return
+        }
+        const decoded = jwtDecode(token)
+        const tokenExpiration = decoded.exp
+        const now = Date.now() / 1000
+        
+        if (tokenExpiration < now){
+            await refreshToken()
+        } else {
+            setIsAuthorized(true)
+        }
+    }
+    
+    if (isAuthorized === null){
+        return <div>Loading...</div>
+    }
+
+    return isAuthorized ? children : <Navigate to="/login" />
+
+    
+}
+export default ProtectedRoute
---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL 
+
+})
+
+api.interceptors.request.use()
+    (config) => {
+        
+    }
