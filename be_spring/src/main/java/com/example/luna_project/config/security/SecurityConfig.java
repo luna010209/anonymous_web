3445a30ade6f787be1c0effeb4f4541c3ec11a59
Do Hoang Huong Lien <luna@Dos-MacBook-Air.local>
2025-09-28 15:29:43 +0900

    Set security config

---
+++ be_spring/src/main/java/com/example/luna_project/config/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.luna_project.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                );
+        return http.build();
+    }
+}
