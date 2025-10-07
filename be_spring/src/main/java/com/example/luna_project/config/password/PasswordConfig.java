eb606a3439d31c021b0fa018954f2e2bd4953b8b
Do Hoang Huong Lien <liendhhha140217@gmail.com>
2025-10-07 11:45:22 +0900

    Config password

---
+++ be_spring/src/main/java/com/example/luna_project/config/password/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.example.luna_project.config.password;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
