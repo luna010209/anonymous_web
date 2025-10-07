0ebb8f21d5c1b04219d9935a075b82f3c2ec7b57
Do Hoang Huong Lien <liendhhha140217@gmail.com>
2025-10-07 11:39:03 +0900

    Entity+Dto+Repo Auth

--- be_spring/src/main/java/com/example/luna_project/service/auth/AuthServiceImpl.java
+++ be_spring/src/main/java/com/example/luna_project/service/auth/AuthServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.luna_project.service.auth;
 
 import com.example.luna_project.dto.auth.SignupRequest;
+import com.example.luna_project.entity.auth.Auth;
 import com.example.luna_project.repo.AuthRepo;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,6 +13,13 @@ public class AuthServiceImpl implements AuthService{
     private final AuthRepo authRepo;
     @Override
     public Long registerUser(SignupRequest request) {
+//        if (authRepo.existsByEmail(request.email()))
+//            throw new BadRequestException("");
+        Auth newUser = Auth.builder()
+                .email(request.email())
+                .phone(request.phone())
+                .username(request.username())
+                .build();
         return 0L;
     }
 }
