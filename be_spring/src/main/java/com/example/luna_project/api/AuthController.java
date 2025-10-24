de52af233f6a53bca040ff0d623dc503958465b0
Do Hoang Huong Lien <liendhhha140217@gmail.com>
2025-10-24 11:30:02 +0700

    Get client ip

--- be_spring/src/main/java/com/example/luna_project/api/AuthController.java
+++ be_spring/src/main/java/com/example/luna_project/api/AuthController.java
@@ -1,14 +1,19 @@
 package com.example.luna_project.api;
 
+import com.example.luna_project.dto.auth.LoginRequest;
+import com.example.luna_project.dto.auth.LoginResponse;
 import com.example.luna_project.dto.auth.SignupRequest;
 import com.example.luna_project.service.auth.AuthService;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +27,28 @@ public class AuthController {
     ){
         Long newId = authService.registerUser(request);
         return ResponseEntity.ok(newId);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @ResponseBody LoginRequest request,
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse
+    ){
+        String ipAddress =
+    }
+
+    public String getClientIp(HttpServletRequest request){
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip==null || ip.isEmpty()) ip = request.getHeader("X-FORWARDED-FOR");
+
+        if (ip==null || ip.isEmpty()) ip = request.getRemoteAddr();
+
+        if (StringUtils.isNotBlank(ip)){
+            List<String> ips = Arrays.asList(ip.split(","));
+            ip = ips.get(0).trim();
+        }
+        return ip;
     }
 }
